2ac102f388df7c412b74f4cf1281abfdf5e7d5cb
Konrad Kęska <keska.konrad@gmail.com>
2019-11-24 23:59:43 +0100

    Fix failing test

--- src/components/__tests__/AlbumTile.js
+++ src/components/__tests__/AlbumTile.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
 
 import AlbumTile from "../Tile/Album";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <AlbumTile
-      album={{ id: 1, title: "test" }}
-      index={1}
-      user="Jan Kowalski"
-    />,
+    <Router history={createMemoryHistory()}>
+      <AlbumTile
+        album={{ id: 1, title: "test" }}
+        index={1}
+        user="Jan Kowalski"
+      />
+    </Router>,
     div,
   );
   ReactDOM.unmountComponentAtNode(div);
